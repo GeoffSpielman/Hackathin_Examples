78b6afe044428d7292cd669e7df1b83b94fdbce2
GeoffSpielman <geoff.spielman21@gmail.com>
2017-10-21 01:09:04 -0400

    Added mouse and keyboard functionality

--- Keyboard_Mouse/Keyboard_Mouse.ino
+++ Keyboard_Mouse/Keyboard_Mouse.ino
@@ -1,3 +1,6 @@
+#include <Keyboard.h>
+#include <Mouse.h>
+
 const int TOGGLE_BTN = 2;
 const int LEFT_BTN = 12;
 const int RIGHT_BTN = 11;              
@@ -14,12 +17,16 @@ void setup() {
   pinMode(LEFT_BTN, INPUT);
   pinMode(RIGHT_BTN, INPUT);
   pinMode(LED, OUTPUT);
+
+  Keyboard.begin();
+  Mouse.begin();
 }
 
 //this code runs continuously
 void loop() {
   
   if (digitalRead(TOGGLE_BTN)){
+    //invert the state of the boolean variable
     toggleOn = !toggleOn;
     if (toggleOn){
       digitalWrite(LED, HIGH);
@@ -31,10 +38,15 @@ void loop() {
   
   if (toggleOn) {
     if (digitalRead(LEFT_BTN)){
-      Serial.println("Read left button");  
+      Keyboard.print("LB\n"); //Keyboard.println() doesn't actually put text on the next line?
+      //keyboard doesn't respond instantaneously, allow 2 millisecond delay
+      delay(2);  
     }
     else if (digitalRead(RIGHT_BTN)){
-      Serial.println("Read right button");
+      //motion onscreen is always relative to the cursor's current location
+      Mouse.move(1, 2, 0); // parameters: xVal, yVal, scroll wheel
+      //mouse doesn't respond instantaneously, allow 2 millisecond delay
+      delay(2);
     }
   }
 }
