6037b71d034f88c40e3c89b9c9ebf1f9953067e7
GeoffSpielman <geoff.spielman21@gmail.com>
2017-10-22 00:38:08 -0400

    Changed example to toggle the LED instead

--- Button_Debouncing/Button_Debouncing.ino
+++ Button_Debouncing/Button_Debouncing.ino
@@ -3,13 +3,25 @@ const int LED = 3;
 
 bool ledOn = false;
 
+bool buttonPressed = false;
+bool buttonReleased = false;
+
 void setup() {
   pinMode(BTN, INPUT);
   pinMode(LED, OUTPUT);
 }
 
 void loop() {
+  
   if (digitalRead(BTN)){
+    buttonPressed = true;
+  }
+
+  //check if button has been released
+  if (!digitalRead(BTN) && buttonPressed){
+    buttonPressed = false;
+    
+    //toggle the state of the LED
     ledOn = !ledOn;
     if (ledOn){
       digitalWrite(LED, HIGH);
