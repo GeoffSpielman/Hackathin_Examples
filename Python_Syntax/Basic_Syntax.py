565f926ad73453119b4503346ce51b383673ca5e
GeoffSpielman <geoff.spielman21@gmail.com>
2017-10-25 00:12:59 -0400

    Partially complete

---
+++ Python_Syntax/Basic_Syntax.py
@@ -0,0 +1,106 @@
+#!/usr/bin/python
+import math
+
+print("hello world")
+
+myName = "Geoff"
+print("Hello " + myName)
+
+a = 3
+b = 5
+print(a + b)
+
+a += 1  # a++ not supported
+print(a)
+
+
+testString = 'pineapple'
+
+if testString == 'pear':
+	print('it\'s a pear')
+
+elif testString == 'pineapple':
+	print('it\'s a pineapple')
+	print('hooray!')
+
+else:
+	print('it\'s no fun')
+
+print("\n=== String Properties ===")
+print("String length: " + str(len(testString))) #+ is text concatenation operator which needs two strings.  len() returns an int
+print("First char: " + testString[0])
+print("Last char: " + testString[-1])
+print("Second last char: " + testString[-2])
+print("First 3 chars: " + testString[0:3])
+print("Third to fifth characters " + testString[2:5])
+print("Every second letter of the entire sting: " + testString[0::2])
+print("Whole string backwards: " + testString[::-1])
+
+# there
+# is
+# no 
+# multi-line
+# comment
+# functionality :(
+
+print("\n=== Functions ===")
+def SquareIt(num):
+	return num*num
+
+def SquareWithDefault(num = 3):
+	return num * num
+
+potato = 4
+print(str(SquareIt(potato)))
+print(str(SquareWithDefault()))
+
+result = SquareIt(SquareIt(SquareIt(potato)))
+print("Big number: " + str(result))
+
+
+print("\n=== Math Operators ===")
+a = -5
+print(abs(a))
+a = 90
+print(math.cos(a))
+a = math.radians(a) # easier to read than (a * 180 / math.pi)
+print(math.cos(a))
+b = 67.365465
+print("Using round: " + str(round(b)))
+print("Using floor: " + str(math.floor(b)))
+print("Using ceil: " + str(math.ceil(b)))
+
+
+
+
+print("\n=== Lists/Arrays ===")
+bob = [2,4,5,3]
+print (bob)
+sam = [2, 'walrus', 9.9234, True, math.pi]
+print(sam)
+
+#auto generate lists from ranges
+incrementalList = range(5)
+print("Ranges are not exactly lists: " + str(incrementalList))
+print("But you can typecast them into one: " + str(list(incrementalList)))
+
+skippingList = range(0, 10, 2)
+print("Skipping list: " + str(list(skippingList)))
+
+emptyList = []
+emptyList.append(-2)
+emptyList.append(-1)
+print(emptyList)
+emptyList = emptyList + list(incrementalList)
+print("You can concantenate lists: " + str(emptyList))
+print("Third item: " + str(emptyList[2])) # never forget indices start at 0
+
+list2D = [[1, 2, 3], [4, 5, 6]]
+print(list2D[0])
+print(list2D[1])
+print('looking for five: ' + str(list2D[1][1]))
+
+
+
+
+print("\n=== Loops ===")
